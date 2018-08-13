aa8945f772777d2640bc9903e9067d4ba82dbf3e
S A D D A M <S A D D A M@Windows10>
2018-08-13 20:54:08 +0700

    Coba method length & isEmpty

--- src/cobaoperasistring/OperasiString.java
+++ src/cobaoperasistring/OperasiString.java
@@ -5,5 +5,15 @@ public class OperasiString {
     public static void main(String[] args) {
         String identitas = "SADDAM RAIHAN RAMADHAN / XRPL 2 / 30";
         System.out.println("identitas : " + identitas);
+        
+        String x = "Operasi";
+        System.out.println("isi variabel x : "+ x);
+        System.out.println("\""+ x + "\" panjang karakter ="+ x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variabel y : "+ y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
        }   
 }
