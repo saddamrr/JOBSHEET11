aee8f928724a9a026f93e77aac5d1e942b6ed195
S A D D A M <S A D D A M@Windows10>
2018-08-13 21:12:41 +0700

    Coba method contains, toUpperCase, toLowerCase, trim&concat

--- src/cobaoperasistring/OperasiString.java
+++ src/cobaoperasistring/OperasiString.java
@@ -40,5 +40,16 @@ public class OperasiString {
         System.out.println("isi variabel s : "+ s);
         System.out.println("perbandingan isi r dengan s (Case Sensitive) : "
                 + r.compareTo(s));
+        
+        System.out.println("pada x terdapat huruf 'a' : " + x.contains("a"));
+        
+        System.out.println("isi variabel x besar semua : "+ x.toUpperCase());
+        System.out.println("isi variabel x kecil semua : "+ x.toLowerCase());
+        
+        String t = "   operasi String   ";
+        System.out.println("isi variabel t : \""+ t +"\"");
+        System.out.println("isi variabel t tanpa spasi : \""+ t.trim() +"\"");
+        
+        System.out.println("gabungkan isi variabel x dengan r : "+ x.concat(r));
        }   
 }
