073f06afec343202f716d9888113b7f5785ec732
S A D D A M <S A D D A M@Windows10>
2018-08-13 19:14:07 +0700

    Initial Commit

---
+++ src/cobaoperasistring/OperasiString.java
@@ -0,0 +1,9 @@
+package cobaoperasistring;
+import java.util.Locale;
+public class OperasiString {
+
+    public static void main(String[] args) {
+        String identitas = "SADDAM RAIHAN RAMADHAN / XRPL 2 / 30";
+        System.out.println("identitas : " + identitas);
+       }   
+}
