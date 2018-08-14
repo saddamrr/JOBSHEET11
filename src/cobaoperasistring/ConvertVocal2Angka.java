77507816db81a39579a10e435ff402a4e49e4ba3
S A D D A M <S A D D A M@Windows10>
2018-08-14 11:16:08 +0700

    Tambah Tampil Per Kata

--- src/cobaoperasistring/ConvertVocal2Angka.java
+++ src/cobaoperasistring/ConvertVocal2Angka.java
@@ -25,11 +25,19 @@ public class ConvertVocal2Angka {
         
         return kalimat;
     }
+    private static void tampilPerkata(String kalimat, String convert) {
+        String[] arrKal = kalimat.split(" ");
+        String[] arrCon = convert.split(" ");
+        
+        for(int i=0;i<arrKal.length;i++)
+            System.out.println(arrKal[i]+" => "+arrCon[i]);
+    }
     public static void main(String[] args) {
         String identitas = "SADDAM RAIHAN RAMADHAN / XRPL2 / 30";
         
         tampilJudul(identitas);
         String kalimat = tampilInput();
         String convert = vocal2Angka(kalimat);
+        tampilPerkata(kalimat, convert);
     }
 }
