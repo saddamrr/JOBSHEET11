65268f911b6162a3ccc8d7ddb8bb4ba6fb890d4e
S A D D A M <S A D D A M@Windows10>
2018-08-14 11:13:51 +0700

    Tambah Input

--- src/cobaoperasistring/ConvertVocal2Angka.java
+++ src/cobaoperasistring/ConvertVocal2Angka.java
@@ -6,10 +6,19 @@ public class ConvertVocal2Angka {
         System.out.println("Identitas : " + identitas);
         System.out.println("\nConvert Kalimat Alay (Vokal Ke Angka)\n");
     }
-    
+    private static String tampilInput() {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Masukkan Kalimat : ");
+        String kalimat = scanner.nextLine();
+        System.out.println("Kalimat Asli : " + kalimat);
+        
+        return kalimat;
+    }
     public static void main(String[] args) {
         String identitas = "SADDAM RAIHAN RAMADHAN / XRPL2 / 30";
         
         tampilJudul(identitas);
+        String kalimat = tampilInput();
     }
 }
